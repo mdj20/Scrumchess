21d83d1ec5fff8816e87e9740cfaffbbbd9300cd
Matthew Jeffreys <mdj20@pitt.edu>
2016-12-30 04:12:52 -0500

    Added player info to game.

--- src/com/scrumchess/data/Game.java
+++ src/com/scrumchess/data/Game.java
@@ -3,12 +3,13 @@ import java.util.Date;
 import com.google.appengine.api.datastore.*;
 public class Game {
 	
-	protected Game(String fen ,Integer moveNum,Date date){
+	protected Game(String fen ,Integer moveNum, Date date){
 		this.fen = fen;
 		this.moveNum = moveNum;
 		this.started = date;
+		this.isBlack = false;
+		this.isWhite = false;
 	}
-	
 	public int getId() {
 		return id;
 	}
@@ -33,9 +34,39 @@ public class Game {
 	protected void setStarted(Date started) {
 		this.started = started;
 	}
+	protected void setIsWhite(boolean isWhite) {
+		this.isWhite = isWhite;
+	}
+	protected void setIsBlack(boolean isBlack) {
+		this.isBlack = isBlack;
+	}
+	protected void setWhite(int white) {
+		this.white = white;
+		this.isWhite = true;
+	}
+	protected void setBlack(int black) {
+		this.black = black;
+		this.isBlack = true;
+	}
+	public boolean isWhite() {
+		return isWhite;
+	}
+	public boolean isBlack() {
+		return isBlack;
+	}
+	public int getWhite() {
+		return white;
+	}
+	public int getBlack() {
+		return black;
+	}
 	private int id;
 	private String fen;
 	private int moveNum;
 	private Date started;
+	private boolean isWhite;
+	private boolean isBlack;
+	private int white;
+	private int black;
 	
 }
--- src/com/scrumchess/data/GameProxy.java
+++ src/com/scrumchess/data/GameProxy.java
@@ -6,6 +6,11 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.datastore.QueryResultList;
 public class GameProxy {
 	protected final static String _kind = "game";
@@ -13,23 +18,45 @@ public class GameProxy {
 	protected final static String _moveNum = "moveNum"; 
 	protected final static String _started = "started";
 	protected final static String _startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 1 1";
+	protected final static String _black = "black";
+	protected final static String _white = "white";
+	protected final static String _isWhite = "isWhite"; 
+	protected final static String _isBlack = "isBlack";
 	private DatastoreService dss;
 	
 	protected GameProxy(DatastoreService dstore){
 		this.dss = dstore;
 	}
-	
-	protected Key newGameToUser(Key parent){
+	protected Key newGameToUserWhite(int user){
 		Key key;
 		Date date = new Date();
 		Game game = new Game(_startFen,0,date);
-		Entity entity = toEntity(game,parent);
+		game.setWhite(user);
+		Entity entity = toEntity(game);
 		key = dss.put(entity);
 		return key;
 	}
-	
-	protected ArrayList<Game> getGames(Key userKey){
-		Query query = new Query(_kind).setAncestor(userKey);
+	protected Key newGameToUserBlack(int user){
+		Key key;
+		Date date = new Date();
+		Game game = new Game(_startFen,0,date);
+		game.setBlack(user);
+		Entity entity = toEntity(game);
+		key = dss.put(entity);
+		return key;
+	}
+	protected Key newGameToUsers(int white,int black){
+		Key key;
+		Date date = new Date();
+		Game game = new Game(_startFen,0,date);
+		game.setWhite(white);
+		game.setBlack(black);
+		Entity entity = toEntity(game);
+		key = dss.put(entity);
+		return key;
+	}
+	protected ArrayList<Game> getGames(int userID){
+		Query query = getBlackOrWhiteQuery(userID);
 		PreparedQuery pq = dss.prepare(query);
 		Iterable<Entity> results;
 		results = pq.asIterable();
@@ -48,18 +75,39 @@ public class GameProxy {
 		}
 		return ret;
 	}	
-	protected Entity toEntity(Game game, Key parent){
-		Entity entity = new Entity(_kind,parent);
-		entity.setProperty(_fen, game.getFen());
-		entity.setProperty(_moveNum, game.getMoveNum());
-		entity.setProperty(_started, game.getStarted());
-		return entity;		
-	}
+
 	protected Entity toEntity(Game game){
 		Entity entity = new Entity(_kind);
 		entity.setProperty(_fen, game.getFen());
 		entity.setProperty(_moveNum, game.getMoveNum());
 		entity.setProperty(_started, game.getStarted());
+		if (game.isWhite()){
+			entity.setProperty(_white, game.getWhite());
+			entity.setProperty(_isWhite, true);
+		}
+		else {
+			entity.setProperty(_isWhite, false);
+		}
+		if (game.isBlack()){
+			entity.setProperty(_white, game.getBlack());
+			entity.setProperty(_isBlack, true);
+		}
+		else {
+			entity.setProperty(_isBlack,false);
+		}
 		return entity;
+	}
+	
+	private Query getBlackOrWhiteQuery(int user){
+		ArrayList<Filter> filterList = new ArrayList<Filter>();
+		filterList.add(getEqualFilter(_black,user));   // calls local method that creates user filter
+		filterList.add(getEqualFilter(_white,user));
+		CompositeFilter cf = new CompositeFilter(CompositeFilterOperator.OR,filterList);
+		Query q = new Query(_kind).setFilter(cf);
+		return q;
+	}
+	private Filter getEqualFilter(String color,int user){
+		Filter filter = new FilterPredicate(color, FilterOperator.EQUAL,user);
+		return filter;
 	}
 }
